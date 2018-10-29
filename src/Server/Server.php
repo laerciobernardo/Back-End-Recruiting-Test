78fa07c06dc7261c225b624f6d539b6eea07191f
Laércio da Rocha Bernardo <laerciobernardo@hotmail.com>
2018-10-28 21:33:00 -0300

    server working

---
+++ src/Server/Server.php
@@ -0,0 +1,111 @@
+<?php
+namespace App\Server;
+
+class Server
+{
+    public static function listen($server)
+    {
+        $method = $server['REQUEST_METHOD'];
+        $url = $server['REQUEST_URI'];
+
+        $param = explode('/', $url);
+        if (empty($param[0])) {
+            array_shift($param);
+        }
+
+        $payload = self::getpayload($server);
+
+        switch ($method) {
+            case 'GET':
+                if (count($param) > 1) {
+                    $method = self::camelize(strtolower('FIND_ONE'));
+                } else {
+                    $method = strtolower('FIND');
+                }
+
+                break;
+            case 'POST':
+                if (!$payload) {
+                    self::handlerpayloadError();
+                    return;
+                }
+                $method = strtolower("CREATE");
+                break;
+            case 'PUT':
+                if (!$payload) {
+                    self::handlerpayloadError();
+                    return;
+                }
+                $method = strtolower("UPDATE");
+                break;
+            case 'DELETE':
+                $method = strtolower("REMOVE");
+                break;
+            default:
+                http_response_code(500);
+                echo json_encode(array('error' => "OMG! We can't handler this action!"));
+                break;
+        }
+        self::handlerController($url, $method, $payload);
+    }
+
+    private function getpayload($server)
+    {
+        if (!$server['HTTP_CONTENT_LENGTH'] || !$server['CONTENT_LENGTH']) {
+            return null;
+        }
+        return file_get_contents('php://input');
+    }
+
+    private function handlerpayloadError()
+    {
+        http_response_code(500);
+        echo json_encode(array('error' => 'Hey dude! The request needs some payload'));
+    }
+
+    private function handlerController($url, $method, $payload)
+    {
+        if ($url === '/') {
+            http_response_code(200);
+            echo json_encode(array(
+                'hello' => 'Hi, if you need more info please let me know on laerciobernardo@hotmail.com',
+                'tasks' => 'http://localhost:8000/task',
+            ));
+            return;
+        }
+
+        $path = explode('/', $url);
+        if (empty($path[0])) {
+            array_shift($path);
+        }
+
+        $className = ucfirst($path[0]);
+        $param = isset($path[1]) ? $path[1] : null;
+
+        $className = "App\Controllers\\".$className;
+
+        echo "<pre>";
+        var_dump(array(
+            'className' => $className,
+            'method' => $method,
+            'param' => $param,
+            'payload' => json_decode($payload, true)
+        ));
+
+
+        if (!class_exists($className)) {
+            http_response_code(500);
+            echo json_encode(array('error' => "OMG! We can't handler this action with this namespace!"));
+            return;
+        }
+
+        $return = call_user_func_array(array(new $className, $method), array($param, json_decode($payload)));
+
+        echo json_encode($return);
+    }
+
+    private function camelize($input, $separator = '_')
+    {
+        return str_replace($separator, '', lcfirst(ucwords($input, $separator)));
+    }
+}
